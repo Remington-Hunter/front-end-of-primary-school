f6ca3841833964df3ddac6664446b0c4b965ef5e
456jbw <1418170527@qq.com>
2021-08-20 20:35:46 +0800

    fix:编辑问卷页面
    fix:编辑问卷页面

--- src/views/questionnaire/NormalQuestion.vue
+++ src/views/questionnaire/NormalQuestion.vue
@@ -2,33 +2,36 @@
   <div>
     <v-card class="mx-auto my-12" max-width="1000">
       <v-card elevation="2" class="title">
-        <span>
-          <v-dialog v-model="dialog_title" width="500">
-            <template v-slot:activator="{ on, attrs }">
-              <span dark v-bind="attrs" v-on="on">{{ title }}</span>
-            </template>
-            <v-card>
-              <v-card-title class="text-h5 grey lighten-2">
-                修改标题
-              </v-card-title>
+        <div>
+          <span>
+            <v-dialog v-model="dialog_title" width="500">
+              <template v-slot:activator="{ on, attrs }">
+                <span dark v-bind="attrs" v-on="on">{{ title }}</span>
+              </template>
+              <v-card>
+                <v-card-title class="text-h5 grey lighten-2">
+                  修改标题
+                </v-card-title>
 
-              <v-card-text>
-                <template>
-                  <v-text-field v-model="title"> </v-text-field>
-                </template>
-              </v-card-text>
+                <v-card-text>
+                  <template>
+                    <v-text-field v-model="title"> </v-text-field>
+                  </template>
+                </v-card-text>
 
-              <v-divider></v-divider>
+                <v-divider></v-divider>
 
-              <v-card-actions>
-                <v-spacer></v-spacer>
-                <v-btn color="primary" text @click="dialog_title = false">
-                  确认
-                </v-btn>
-              </v-card-actions>
-            </v-card>
-          </v-dialog>
-        </span>
+                <v-card-actions>
+                  <v-spacer></v-spacer>
+                  <v-btn color="primary" text @click="dialog_title = false">
+                    确认
+                  </v-btn>
+                </v-card-actions>
+              </v-card>
+            </v-dialog>
+          </span>
+        </div>
+
         <div>
           <span>
             <v-dialog v-model="dialog_describe" width="500">
@@ -58,25 +61,43 @@
             </v-dialog>
           </span>
         </div>
-      </v-card>
-      <v-card >
-        <v-card-title>
-            题目控件
-        </v-card-title>
-        <div v-for="(item,index) in problem_list" :key="(item,index)">
-        <v-btn>
-            <v-icon>{{item.icon}}</v-icon>
-            {{item.text}}
-        </v-btn>
+
+        <div v-for="(item, index) in created_problem" :key="(item, index)">
+          <SingleSelect
+            :problem_type="item"
+            :problem_number="index + 1"
+            @CancelNewProblem="
+              created_problem.pop();
+              is_creating = false;
+            "
+            @ConfirmProblem="is_creating = false"
+            @deleteProblem="deleteProblem"
+          ></SingleSelect>
         </div>
-    </v-card>
+      </v-card>
+      <v-card>
+        <v-card-title>
+          题目控件
+        </v-card-title>
+        <div v-for="(item, index) in problem_list" :key="(item, index)">
+          <v-btn @click="newProblem(item.text)">
+            <v-icon>{{ item.icon }}</v-icon>
+            {{ item.text }}
+          </v-btn>
+        </div>
+      </v-card>
     </v-card>
   </div>
 </template>
 
 <script>
+import SingleSelect from "../../components/SingleSelect";
+
 export default {
   name: "NormalQuestion",
+  components: {
+    SingleSelect,
+  },
   data() {
     return {
       title: "添加标题",
@@ -85,34 +106,33 @@ export default {
       describe: "添加说明",
       dialog_describe: false,
 
-      problem_list:[
-          {text:'单选题',icon:'mdi-album'},
-          {text:'多选题',icon:'mdi-check-bold'},
-          {text:'单选题',icon:'mdi-alpha-i-box'}
-      ]
+      problem_list: [
+        { text: "单选题", icon: "mdi-album" },
+        { text: "多选题", icon: "mdi-check-bold" },
+        { text: "填空题", icon: "mdi-alpha-i-box" },
+        { text: "评分题", icon: "mdi-alpha-i-box" },
+      ],
+
+      created_problem: [],
+      is_creating: false,
     };
   },
   methods: {
     changeTitle() {
       this.is_change_title = !this.is_change_title;
     },
+    newProblem(index) {
+      if (!this.is_creating) {
+        this.created_problem.push(index);
+        this.is_creating = true;
+      }
+    },
+    deleteProblem(index) {
+      console.log(index);
+      this.created_problem.splice(index-1,1)
+    },
   },
-//   created(){
-//       axios({
-//         url:'http://82.157.97.70/api/login_username_password',
-//         method:'post',
-//         params:{
-//           password:'123',
-//           username:'this.form.nickname'
-//         },
-//         // data:{}
-//         
-//       }).then((res)=>{
-//         console.log(res)
-//         this.$router.push('/')
-//       })
-//   }
-}
+};
 </script>
 
 <style scoped></style>
