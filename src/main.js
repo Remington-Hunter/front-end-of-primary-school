1161d2783bd1867865bdbe55aff7d7d1ecfb517a
buaaer-stack <2550806098@qq.com>
2021-08-20 15:44:59 +0800

    关于localStore的一些修改

--- src/main.js
+++ src/main.js
@@ -4,14 +4,22 @@ import router from './router'
 import store from './store'
 import vuetify from './plugins/vuetify'
 import Element from 'element-ui'
-import 'element-ui/lib/theme-chalk/index.css' 
+import 'element-ui/lib/theme-chalk/index.css'
 
 Vue.use(Element)
 Vue.config.productionTip = false
+    //自动给同一个vue项目的所有请求添加请求头
+axios.interceptors.request.use(function(config) {
+    let token = localStorage.getItem('authorization');
+    if (token) {
+        config.headers['Authorization'] = token;
+    }
+    return config;
+})
 
 new Vue({
-  router,
-  store,
-  vuetify,
-  render: h => h(App)
+    router,
+    store,
+    vuetify,
+    render: h => h(App)
 }).$mount('#app')
