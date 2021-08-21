99b85ec29823a1d9864add4a978ba3a5d30fc395
buaaer-stack <2550806098@qq.com>
2021-08-21 09:45:12 +0800

    生成pdf

--- src/main.js
+++ src/main.js
@@ -6,7 +6,8 @@ import vuetify from './plugins/vuetify'
 import Element from 'element-ui'
 import 'element-ui/lib/theme-chalk/index.css'
 import './assets/css/icon/iconfont.css'
-
+import htmlToPdf from '@/assets/js/htmlToPdf'
+Vue.use(htmlToPdf);
 Vue.use(Element)
 Vue.config.productionTip = false
     //自动给同一个vue项目的所有请求添加请求头
