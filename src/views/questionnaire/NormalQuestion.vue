fe1ef6d7835c034d4d7d8e785d77597dd89327fb
buaaer-stack <2550806098@qq.com>
2021-08-21 13:59:49 +0800

    1

--- src/views/questionnaire/NormalQuestion.vue
+++ src/views/questionnaire/NormalQuestion.vue
@@ -110,13 +110,6 @@
         </v-btn>
       </div>
     </v-card>
-    <v-card>
-      <v-dialog v-model="isPreview">
-         <v-card-title>
-           问卷预览
-      </v-card-title>
-      </v-dialog>
-    </v-card>
   </div>
 </div>
   
