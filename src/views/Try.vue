7b0a32e3ec441e070d6c5051d73995df3ed55359
renming-lab <70315185+renming-lab@users.noreply.github.com>
2021-08-26 16:38:09 +0800

    地图定位

--- src/views/Try.vue
+++ src/views/Try.vue
@@ -1,134 +1,106 @@
-<!--<template>-->
-<!--  <div>-->
-<!--    <span>{{city}}</span>-->
-<!--    <span>{{location}}</span>-->
-<!--  </div>-->
-
-<!--</template>-->
-
-<!--<script>-->
-<!--import BMap from 'BMap'-->
-<!--import BMapLib from 'BMapLib'-->
-<!--export default {-->
-<!--  data(){-->
-<!--    return{-->
-<!--      city:'',-->
-<!--      location:''-->
-<!--    }-->
-<!--  },-->
-<!--  // methods部分-->
-<!--  methods:{-->
-<!--    getLocation() {-->
-<!--      const self = this-->
-<!--      AMap.plugin('AMap.Geolocation', function () {-->
-<!--        var geolocation = new AMap.Geolocation({-->
-<!--          // 是否使用高精度定位，默认：true-->
-<!--          enableHighAccuracy: true,-->
-<!--          // 设置定位超时时间，默认：无穷大-->
-<!--          timeout: 10000,-->
-<!--        })-->
-
-<!--        geolocation.getCurrentPosition()-->
-<!--        AMap.event.addListener(geolocation, 'complete', onComplete);-->
-<!--        AMap.event.addListener(geolocation, 'error', onError);-->
-
-<!--        function onComplete(data) {-->
-<!--          // data是具体的定位信息-->
-<!--          console.log('定位成功信息：', data);-->
-<!--        }-->
-<!--        function onError(data) {-->
-<!--          // 定位出错-->
-<!--          console.log('定位失败错误：', data);-->
-<!--          // 调用ip定位-->
-<!--          self.getLngLatLocation();-->
-<!--        }-->
-<!--      })-->
-<!--    },-->
-<!--    getLngLatLocation() {-->
-<!--      var self = this;-->
-<!--      AMap.plugin('AMap.CitySearch', function () {-->
-<!--        var citySearch = new AMap.CitySearch();-->
-<!--        citySearch.getLocalCity(function (status, result) {-->
-<!--          if (status === 'complete' && result.info === 'OK') {-->
-<!--            // 查询成功，result即为当前所在城市信息-->
-<!--            console.log('通过ip获取当前城市：', result)-->
-<!--            self.city = result.city;-->
-<!--            //逆向地理编码-->
-<!--            AMap.plugin('AMap.Geocoder', function () {-->
-<!--              var geocoder = new AMap.Geocoder({-->
-<!--                // city 指定进行编码查询的城市，支持传入城市名、adcode 和 citycode-->
-<!--                city: result.adcode-->
-<!--              })-->
-<!--              var lnglat = result.rectangle.split(';')[0].split(',');-->
-<!--              geocoder.getAddress(lnglat, function (status, data) {-->
-<!--                if (status === 'complete' && data.info === 'OK') {-->
-<!--                  // result为对应的地理位置详细信息-->
-<!--                  console.log(data)-->
-<!--                  self.location = data;-->
-<!--                }-->
-<!--              })-->
-<!--            })-->
-<!--          }-->
-<!--        })-->
-<!--      })-->
-<!--    },-->
-<!--  },-->
-<!--// created 中调用-->
-<!--  created () {-->
-<!--    // 此处为调用精确定位之后，调取ip定位，可根据实际情况改写-->
-<!--    this.getLocation();-->
-<!--  }-->
-
-<!--}-->
-<!--</script>-->
-
 <template>
   <div>
-    <div class="home">
-      <div>您现在所在的城市为：{{location}}</div>
+    <div class="locationHeader">
+      <div class="region">{{ locationDescription }}</div>
+      <div style="display: flex; align-items: center">
+              <span
+                  style="font-size: 20px; margin-left: 10px; margin-right: 5px; color: #444444;"
+              >
+                <i class="el-icon-location"></i>
+                当前位置
+              </span>
+      </div>
     </div>
-  </div>
+    </div>
 </template>
 
-
-
 <script>
-import BMap from 'BMap'
-
+import axios  from "axios";
 export default {
+  name: 'Location',
+  mounted () {
+    this.getLocation()
+  },
   data() {
-    return{
-      location: null
-    }
+    return {
+      // locationInfo: {
+      //   ip: '',
+      //   country: '',
+      //   province: '',
+      //   city: '',
+      //   district: '',
+      //   location: '',
+      // },
+      locationInfo: {
+        ip: '',
+        country: '中国',
+        province: '北京市',
+        city: '北京市',
+        district: '海淀区',
+        location: '116.310316,39.956074',
+      },
+    };
   },
-  methods:{
-    get_location(){
-      let _this = this
-      var geolocation = new BMap.Geolocation()
-      geolocation.getCurrentPosition(function(r) {
-        if (this.getStatus() === BMAP_STATUS_SUCCESS) {
-          const myGeo = new BMap.Geocoder()
-          myGeo.getLocation(new BMap.Point(r.point.lng, r.point.lat), data => {
-            if (data.addressComponents) {
-              const result = data.addressComponents
-              console.log(result)
-              _this.location = {
-                creditLongitude: r.point.lat, // 经度
-                creditLatitude: r.point.lng, // 纬度
-                creditProvince: result.province || '', // 省
-                creditCity: result.city || '', // 市
-                creditArea: result.district || '', // 区
-                creditStreet: (result.street || '') + (result.streetNumber || '') // 街道
-              }
-            }
-          })
+  computed: {
+    locationDescription () {
+      if (this.locationInfo.country === '中国') {
+        if (this.locationInfo.province === this.locationInfo.city) {
+          return this.locationInfo.city + this.locationInfo.district
+        } else {
+          return this.locationInfo.province + this.locationInfo.city + this.locationInfo.district
         }
-      })
-    }
+      } else {
+        return this.locationInfo.country
+      }
+    },
   },
-  mounted() {
-      this.get_location();
-  }}
+  methods: {
+    getLocation() {
+      // eslint-disable-next-line
+      this.locationInfo.ip = window.localStorage.getItem('Ip')
+      console.log(this.locationInfo.ip)
+      var _this = this;
+      var url1 = "https://restapi.amap.com/v5/ip?key=096748afcd44f7a9939d2065057b26ef&type=4&ip=114.247.50.2";
+      axios.get("https://restapi.amap.com/v5/ip?key=a593d64ab73229be6b3d1ef802b76849&type=4&ip="+this.locationInfo.ip)
+          .then(response => {
+            console.log(response)
+            _this.locationInfo.country = response.data.country
+            _this.locationInfo.province = response.data.province
+            _this.locationInfo.city = response.data.city
+            _this.locationInfo.district = response.data.district
+            _this.locationInfo.location = response.data.location
+            console.log(_this.locationInfo)
+          })
+    },
+  }
+}
+
 </script>
 
+<style scoped>
+.locationHeader {
+  display: flex;
+  justify-content: center;
+  align-items: flex-end;
+  margin-bottom: 20px;
 
+  align-self: start;
+}
+.region {
+  white-space: nowrap;
+  text-align: center;
+
+  font-size: 27px;
+  /* font-weight: 500; */
+
+  background-color: #e0e0e0;
+  color: rgb(105, 105, 105);
+
+  /* border: #cccccc solid 2px; */
+  border-radius: 30px;
+
+  padding: 5px 15px 5px 15px;
+  margin: 3px;
+}
+
+</style>
