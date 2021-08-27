1a07bc51603f59aa34df7cfadc1af0e4ce67458a
937547764 <937547764@qq.com>
2021-08-28 00:16:43 +0800

    Update Normal1.vue

--- src/views/questionnaire/Normal1.vue
+++ src/views/questionnaire/Normal1.vue
@@ -203,7 +203,7 @@
               $emit('problem_store');"
             ></SingleSelect>
           </div>
-          <div style="height:45px"></div>
+          <div style="height:36px"></div>
         </div>
       </el-container>
     </el-container>
