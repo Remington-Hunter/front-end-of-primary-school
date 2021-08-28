f3a82ff6fa0a4a3b4fb51ef487b0426d6ff10d57
456jbw <1418170527@qq.com>
2021-08-28 21:50:20 +0800

    Update CountDown.vue

--- src/components/CountDown.vue
+++ src/components/CountDown.vue
@@ -19,7 +19,7 @@
     },
     mounted () {
       if (this.remainTime > 0) {
-        this.hour = Math.floor((this.remainTime / 3600) % 24)
+        this.hour = Math.floor((this.remainTime / 3600) )
         this.minute = Math.floor((this.remainTime / 60) % 60)
         this.second = Math.floor(this.remainTime % 60)
         this.countDowm()
